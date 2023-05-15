6e65508a0fe1a81200b510513813605e8aece6af
mamouros99 <tiago.mamouros@tecnico.ulisboa.pt>
2023-05-15 15:18:27 +0100

    Implmented with docker-compose

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
-FROM ubuntu:latest
-LABEL authors="tiago"
+FROM eclipse-temurin:17-jdk-alpine AS build
+WORKDIR /app
+COPY . /app
+RUN --mount=type=cache,target=/root/.gradle ./gradlew clean build
 
-ENTRYPOINT ["top", "-b"]
+
+FROM eclipse-temurin:17-jdk-alpine AS deployment
+WORKDIR /app
+LABEL authors="mamouros99"
+COPY build/libs/*.jar app.jar
+ENTRYPOINT ["java", "-jar", "/app/app.jar"]
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-/*insert into users (username, name, email, role) values
-                                                    ('ist112345','jorge manel', 'jorgemanel@tecnico.ulisboa.pt', 'VIEWER'),
-                                                    ('ist112346','Miguel Maia', 'Miguelmaia@tecnico.ulisboa.pt', 'EDITOR');
-*/
+/*insert into eco_islands (id, building_name, floor, description, bins) values 
+                                                    (1,'build1','1','desc1','00'),
+                                                    (2,'build2','2','desc2','01'),
+                                                    (3,'build3','3','desc3','10'),
+                                                    (4,'build4','4','desc4','11');*/
